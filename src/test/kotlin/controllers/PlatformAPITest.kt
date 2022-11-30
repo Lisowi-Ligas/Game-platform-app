6856b00fe99b601f90c10f4589a16cc8490c9f4e
lisow <lisekligas@gmail.com>
2022-11-30 15:34:34 +0000

    PlatformAPITest 1 for Game-platform-app

---
+++ src/test/kotlin/controllers/PlatformAPITest.kt
@@ -0,0 +1,58 @@
+package controllers
+
+import models.Platform
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class PlatformAPITest {
+
+    private var Windows: Platform? = null
+    private var Linux: Platform? = null
+    private var Mac: Platform? = null
+    private var Xbox: Platform? = null
+    private var Playstation: Platform? = null
+    private var populatedPlatforms: PlatformAPI? = PlatformAPI()
+    private var emptyPlatforms: PlatformAPI? = PlatformAPI()
+
+    @BeforeEach
+    fun setup(){
+        Windows = Platform("Windows", "PC", "1200 Euro", 7,10,false)
+        Linux = Platform("Linux", "PC", "1200 Euro",5,9, false)
+        Mac = Platform("Mac", "Apple", "2000 Euro", 6, 1,false)
+        Xbox = Platform("Xbox", "XboxSeriesX", "500 Euro", 7,5,false)
+        Playstation = Platform("Playstation", "5", "500", 7,5,false)
+
+        //adding 5 Note to the notes api
+        populatedPlatforms!!.add(Windows!!)
+        populatedPlatforms!!.add(Linux!!)
+        populatedPlatforms!!.add(Mac!!)
+        populatedPlatforms!!.add(Xbox!!)
+        populatedPlatforms!!.add(Playstation!!)
+    }
+
+    @AfterEach
+    fun tearDown(){
+        Windows = null
+        Linux = null
+        Mac = null
+        Xbox = null
+        Playstation = null
+        populatedPlatforms = null
+        emptyPlatforms = null
+    }
+
+    @Test
+    fun `adding a Platform to a populated list adds to ArrayList`(){
+        val newPlatform = Platform("NintendoSwitch", "NintendoSwitch", "200 Euro", 3,5,false)
+        assertTrue(populatedPlatforms!!.add(newPlatform))
+    }
+
+    @Test
+    fun `adding a Platform to an empty list adds to ArrayList`(){
+        val newPlatform = Platform("NintendoSwitch", "NintendoSwitch", "200 Euro", 3,5,false)
+        assertTrue(emptyPlatforms!!.add(newPlatform))
+    }
+}
